b88ca3e5e93119849926cfbcbe8b1e8665d3c65f
ghabrielferrari <ghcferrari@gmail.com>
2025-10-08 11:18:56 -0300

    arrumando erro do macaco

--- TesteIOS/AppDelegate.swift
+++ TesteIOS/AppDelegate.swift
@@ -2,27 +2,36 @@
 //  AppDelegate.swift
 //  TesteIOS
 //
-//  Created by Gabriel Ferrari on 07/10/25.
+//  Created by Gabriel Ferrari on 08/10/25.
 //
 
 import UIKit
 import FirebaseCore
 import FirebaseFirestore
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
-    ) -> Bool {
-        if FirebaseApp.app() == nil {
-            FirebaseApp.configure()
-        }
-
-        // evita confusao de cache para testes
-        let settings = FirestoreSettings()
-        settings.isPersistenceEnabled = false
-        Firestore.firestore().settings = settings
-
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // start firebase
+        FirebaseApp.configure()
+        
+        // instancia do firestore/
+        let db = Firestore.firestore()
+        
+        print(db)
+        
         return true
     }
+    
+    func applicationWillResignActive(_ application: UIApplication) {}
+
+    func applicationDidEnterBackground(_ application: UIApplication) {}
+
+    func applicationWillEnterForeground(_ application: UIApplication) {}
+
+    func applicationDidBecomeActive(_ application: UIApplication) {}
+
+    func applicationWillTerminate(_ application: UIApplication) {}
+    
 }
