754c9f794e9338c6f5806d93a2c3a3578d61b9b7
ghabrielferrari <ghcferrari@gmail.com>
2025-10-09 11:18:14 -0300

    rebase main

--- TesteIOS/AppDelegate.swift
+++ TesteIOS/AppDelegate.swift
@@ -2,36 +2,27 @@
 //  AppDelegate.swift
 //  TesteIOS
 //
-//  Created by Gabriel Ferrari on 08/10/25.
+//  Created by Gabriel Ferrari on 07/10/25.
 //
 
 import UIKit
 import FirebaseCore
 import FirebaseFirestore
 
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // start firebase
-        FirebaseApp.configure()
-        
-        // instancia do firestore/
-        let db = Firestore.firestore()
-        
-        print(db)
-        
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        if FirebaseApp.app() == nil {
+            FirebaseApp.configure()
+        }
+
+        // evita confusao de cache para testes
+        let settings = FirestoreSettings()
+        settings.isPersistenceEnabled = false
+        Firestore.firestore().settings = settings
+
         return true
     }
-    
-    func applicationWillResignActive(_ application: UIApplication) {}
-
-    func applicationDidEnterBackground(_ application: UIApplication) {}
-
-    func applicationWillEnterForeground(_ application: UIApplication) {}
-
-    func applicationDidBecomeActive(_ application: UIApplication) {}
-
-    func applicationWillTerminate(_ application: UIApplication) {}
-    
 }
